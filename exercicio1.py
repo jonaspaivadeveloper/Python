48dbf6eaed22090c8ac82a766378237ef0dcbffd
Jonaspavao <43683098+Jonaspavao@users.noreply.github.com>
2018-10-03 18:12:13 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+largura=17
+altura=12.0
+delimitador="."
+
+largura/2
+largura/2.0
+altura/3
+1+2*5
+delimitador*5
+print(largura/2)
+print(largura/2.0)
+print(altura/3)
+print(1+2*5)
+print(delimitador*5)
---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+import math
+pi=3.1415
+r=5
+#Volume_da_esfera=(4/3)*math.pi*r*r*r 
+Volume_da_esfera=(4/3)*math.pi*math.pow(r,3)
+
+print(Volume_da_esfera)
+
+
