a8418edee6a32149179001dfde15472150ea89ce
Jonaspavao <43683098+Jonaspavao@users.noreply.github.com>
2018-10-05 15:12:02 -0300

    Add files via upload

---
+++ Aula3/exemplo1.py
@@ -0,0 +1,36 @@
+def print_poema():
+    print('caminate, no hay camino')
+    print('se hace camino al andar')
+
+type(print_poema)
+
+print_poema()
+
+def verso_poema():
+    print('caminate, sontus huellas')
+    print('el camino y nada mas')
+
+    print_poema()
+
+    print('al andar se hace el camino')
+
+verso_poema()
+
+def print_poema():
+    print('Caminante, no hay camino,')
+    print('se hace camino al andar.')
+    
+def verso_poema():
+    print('Caminante, son tus huellas')
+    print('el camino y nada más;')
+    
+    print_poema()
+    
+    print('Al andar se hace el camino,')
+    print('y al volver la vista atrás,')
+    print('se ve la senda que nunca')
+    print('se ha de volver a pisar.')
+    print('Caminante no hay camino')
+    print('sino estelas en la mar.')
+
+verso_poema()
---
+++ Aula3/exercicio1.py
@@ -0,0 +1,59 @@
+import math
+#Exercicio1
+def y(arg):
+    print(arg,type(arg))
+
+y(2)
+
+#Exercicio2
+def v(t):
+    print(s/t)
+
+s=2 #distancia
+
+v(2)
+
+def v(t):
+    print(g*t)
+
+g=10 #aceleração(m/s²)
+
+v(2)
+
+#Exercico3
+def theta(h,d):
+    x=h/d
+    print(x)
+    print(math.atan(x))
+    return x
+
+theta(5,0.5)
+
+#Exercicio4
+def metros(milha):
+    y=milha/1609.34
+    print(y)
+    
+metros(1)
+
+def milha(metros):
+    x=metros*1609.34
+    print(x)
+
+milha(1)
+
+def horas(minutos):
+    y=minutos/60
+    print(y)
+horas(30) 
+
+print v=metrs(4)/horas(30)
+    
+
+
+
+
+
+
+
+
